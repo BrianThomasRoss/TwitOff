6de49d182bd7e1ce83e8ff83826b300166bccf77
brianthomasross <brianthomasross@users.noreply.github.com>
2019-12-17 16:58:10 -0500

    BTR Twitter Functionality
    API Keys from .env and logic

---
+++ twitoff/twitter.py
@@ -0,0 +1,51 @@
+import basilica
+import tweepy
+from dotenv import load_dotenv
+from decouple import config
+from .models import DB, Tweet, Tweeter
+
+load_dotenv()
+
+TWITTER_AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_API_KEY'),
+                                   config('TWITTER_CONSUMER_API_KEY_SECRET'))
+
+TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),
+                              config('TWITTER_ACCESS_TOKEN_SECRET')) 
+
+TWITTER = tweepy.API(TWITTER_AUTH)
+
+BASILICA = basilica.Connection(config('BASILICA_KEY'))
+
+def add_or_update_tweeter(username):
+    """ adds or updates user and their tweets"""
+    try:
+        twitter_user = TWITTER.get_user(username)
+        db_tweeter = (Tweeter.query.get(twitter_user.id) or
+                      Tweeter(id=twitter_user.id, handle=username))
+        DB.session.add(db_tweeter)
+        tweets = twitter_user.timeline(
+            count=250, exclude_replies=True, include_rts=False,
+            tweet_mode='extended', since_id=db_tweeter.newest_tweet_id
+        )
+
+        if tweets:
+            db_tweeter.newest_tweet_id = tweets[0].id
+        for tweet in tweets:
+            embedding = BASILICA.embed_sentence(tweet.full_text,
+                                                model='twitter')
+
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300], embedding=embedding)
+            db_tweeter.tweets.append(db_tweet)
+            DB.session.add(db_tweet)
+            
+
+    except Exception as e:
+        # something else
+        print("Error processing {}: {}".format(username, e))
+        raise e
+    else:
+        DB.session.commit()
+
+def update_all():
+    for tweeter in Tweeter.query.all():
+        add_or_update_tweeter(tweeter.handle)
