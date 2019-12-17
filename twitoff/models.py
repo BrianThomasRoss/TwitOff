6ceacaf7c0ee58f2228f16411ae26c0245a4fda8
brianthomasross <brianthomasross@users.noreply.github.com>
2019-12-17 16:58:37 -0500

    Update models.py

--- twitoff/models.py
+++ twitoff/models.py
@@ -1,13 +1,24 @@
+"""SQL alchemy models for tweettweet"""
 from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
-class User(DB.Model):
-    # SQLAlchemy has an implicit constructor for all models no need to define __init__
-    id = DB.Column(DB.Integer, primary_key=True)
-    name = DB.Column(DB.String(20), nullable=False)
+class Tweeter(DB.Model):
+    """Twitter users that we pull and analyze tweets for"""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    handle = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
+
+    def __repr__(self):
+        return "<Tweeter {}>".format(self.handle)
 
 class Tweet(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(280), nullable=False)
+    """Tweets tweeted from the tweeters"""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300))
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    tweeter_id = DB.Column(DB.BigInteger, DB.ForeignKey('tweeter.id'), nullable=False)
+    tweeter = DB.relationship('Tweeter', backref=DB.backref('tweets', lazy=True))
 
+    def __repr__(self):
+        return "<Tweet {}>".format(self.text)
