e083877797a60e2c073845bb45277d8d3869303e
brianthomasross <brianthomasross@users.noreply.github.com>
2019-12-19 11:59:35 -0500

    BTR Fixed Model

--- twitoff/predict.py
+++ twitoff/predict.py
@@ -21,7 +21,7 @@ def predict_tweeter(tweeter1_handle, tweeter2_handle, tweet_text):
 
     
     predictions = log_reg.predict_proba(np.array(tweet_embedding).reshape(1, -1))[0]
-    result = {tweeter1_handle: round(predictions[1]*100, 2),
-              tweeter2_handle: round(predictions[0]*100, 2)}
+    result = {tweeter1_handle: round(predictions[0]*100, 2),
+              tweeter2_handle: round(predictions[1]*100, 2)}
     
     return result
